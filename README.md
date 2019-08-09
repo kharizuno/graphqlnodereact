06122705b3178b5dba78a361c6461899e73d9596
Kharizuno <kharizuno48@gmail.com>
2019-08-09 14:08:17 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## App NodeJS
+express, express-graphql, axios, 
+
+## App React
+react, react-apollo, graphql-tag
+
+### Installation
+npm install
+
+### Run
+npm run dev
---
+++ public/precache-manifest.1840cd33cdb5111b3c3ccedfa7cf5a57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93bad5aabcb05d791fba8c187df983a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3845719f229291c0c8ba",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "77a47b4082dfe3a33040",
+    "url": "/static/js/2.12efe8ed.chunk.js"
+  },
+  {
+    "revision": "3845719f229291c0c8ba",
+    "url": "/static/js/main.cbb8dfab.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6d245f5352285a518f4e6716b74e7924",
+    "url": "/static/media/logo.6d245f53.png"
+  }
+]);
